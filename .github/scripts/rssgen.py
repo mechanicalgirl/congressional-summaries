9e06263d8d128984dc6f8da7656fe569e81d066a
Barbara Shaurette <barbara@bloodredrose.com>
2026-03-16 19:47:21 -0700

    add rss gen

---
+++ .github/scripts/rssgen.py
@@ -0,0 +1,48 @@
+from feedgen.feed import FeedGenerator
+import markdown
+
+from datetime import datetime, timezone
+
+
+today = datetime.now().strftime('%Y-%m-%d')
+todaysfile = f"summaries/{today}.md"
+
+count = 0
+url = ''
+title = ''
+body = ''
+with open(todaysfile, 'r') as file:
+    for line in file:
+        count += 1
+        if count == 3:
+            url += line.strip()
+        if count == 5:
+            title += line.strip()
+        if count >= 7:
+            body += line.strip()+'\n'
+        # print("Line{}: {}".format(count, line.strip()))
+
+content = markdown.markdown(body)
+datetime_obj = datetime.strptime(today, '%Y-%m-%d')
+datetime_obj = datetime_obj.replace(tzinfo=timezone.utc)
+
+fg = FeedGenerator()
+fg.id('https://www.mechanicalgirl.com')
+fg.title(title)
+fg.author({'name':'Barbara','email':'barbara@mechanicalgirl.com'})
+fg.link(href='https://www.mechanicalgirl.com', rel='alternate')
+fg.subtitle('The RSS feed from mechanicalgirl.com')
+fg.language('en')
+
+fe = fg.add_entry(order="append")
+fe.id()
+fe.title(title)
+fe.guid(url)
+fe.link(href=url)
+fe.description(title)
+fe.content(content, type='html')
+fe.published(datetime_obj)
+
+fg.rss_file('rss.xml')
+rssfeed = fg.rss_str()
+
